35f8bbe8ff2dde929d1f118f5cf86c7b890fc476
chaimaeboultam <chaimaeboultam2002@gmail.com>
2024-03-04 12:47:16 -0800

    .

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# creates a file in /tmp
-
-file { '/tmp/school':
-  content =>'I love Puppet',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
+# Create a file if it doesn't exist, and enter a character string.
+file {'/tmp/school':
+ensure  => present,
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 }
 
